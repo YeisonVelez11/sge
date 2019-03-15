e815fc1c3e4a67abba80754d21d4cdf09c0ad865
prjchec.yvelez@umanizales.edu.co <prjchec.yvelez@umanizales.edu.co>
2019-03-15 17:51:47 -0500

    dsadsa

--- pyhton/emotion2_hasta_ids.py
+++ pyhton/emotion2_hasta_ids.py
@@ -25,13 +25,22 @@ punctuation = [",", ";", ".", "..","...", ",.", "...."] # The tokens that you wa
 def fn_limpiarPalabra(palabra):
     json_palabra={"lemma":"", "categoria":""}
     aSeparar=palabra.split("&3&3&3&")
+    print(palabra)
     if len(aSeparar)>1:
         aSeparar=aSeparar[1].replace("%3B","%23")
         aSeparar=aSeparar.replace("&3&","%23")
         aSeparar=aSeparar.split("%23")
-        json_palabra["lemma"]=aSeparar[0]
-        json_palabra["categoria"]=aSeparar[1]
-        json_palabra["categoria"]=json_palabra["categoria"].replace("+"," ")
+        print(aSeparar)
+        
+        if len(aSeparar)>2:
+          for i, item in enumerate(aSeparar):
+            if aSeparar[i].lower().find("adjetivo")!=-1:
+              json_palabra={ "lemma":aSeparar[i-1], "categoria":aSeparar[i]};
+            else:
+                json_palabra["lemma"]=aSeparar[0]
+                json_palabra["categoria"]=aSeparar[1]
+            json_palabra["categoria"]=json_palabra["categoria"].replace("+"," ")             
+
         #print(json_palabra)
         return json_palabra
     else:
@@ -51,10 +60,7 @@ with open('stopWords.csv', newline='') as File:
 
 aCorpusCiad= []
 
-def has_alphanum(str):
-    return token.lower().islower() or any(character.isdigit() for character in token)
-palabras_tokenizadas=['esto', 'es', 'un', 'texto', ',', 'para', 'el', 'sr.', 'gómez', '.', 'Esto', ';', 'es', 'una', 'prueba', 'que', 'puedes', 'encontrar', 'en', 'www.google.com','quiero','encontarla..']
-res = [token for token in palabras_tokenizadas if has_alphanum(token)]
+
 #with open('CorpusCIAD.csv', newline='') as File:  
 with open('nuevo_corpus.csv', newline='') as File:  
  
@@ -96,8 +102,12 @@ with open('chatprueba.csv') as csv_leido:
                 
                 #TOKENIZAR PALABRAS
                 palabras_tokenizadas=(word_tokenize(texto,"spanish"))
-                palabras_tokenizadas = [ palabra for palabra in palabras_tokenizadas if palabra not in punctuation ]
-                print(palabras_tokenizadas)
+                palabras_tokenizadas=[s.rstrip('.;, ') for s in palabras_tokenizadas]
+                
+                
+                #manera de eliminar caracteres a partir de un array
+                #palabras_tokenizadas = [ palabra for palabra in palabras_tokenizadas if palabra not in punctuation ]
+                
                 #freq = nltk.FreqDist(palabras_tokenizadas)
 #                for idx,palabra in enumerate(palabras_tokenizadas):
 #                    #palabras terminadas en rle
@@ -125,62 +135,53 @@ with open('chatprueba.csv') as csv_leido:
                 texto=' '.join(aFrase_sin_stopwords)
                 #print(aFrase_sin_stopwords)
                 
-                
+                print(aFrase_sin_stopwords)
+
                 
                 #convierte un array en un string ' '.join(new_sentence) esto lo pide nlp(text)
                 aFraseLematizada=[];
                 aLemmasOracion=[]
                 
-                for idx, token in enumerate(nlp(texto)):
-                #for token in nlp(' '.join(aFrase_sin_stopwords)):
-                    #queremos que sólo me retorne las palabras, puede darse el csao que no se tokenicen algunas palabras como "de" ?de acuerdo
-                    if token.pos_!="PUNCT":
-                            token_tmp={"texto":token.text, "lemma":token.lemma_, "tipo":token.pos_ }
-                #            if token.pos_ == "VERB" and token.lemma_==token.text and token.lemma_[-1:]!="r":
-                #                print("esta malo el verbo, no se reconoció bien")
-                #                iPeticionWebFirstTime=iPeticionWebFirstTime+1
-                #                if iPeticionWebFirstTime==1:
-                #                    getIdPeticion= requests.post(url, headers = cabecera1, data = {"url":"/lematizador.html","version":"2.1.1","jaxurl":"JaxcentServlet" })
-                #                    idPeticion=getIdPeticion.text.split("&")
-                #                    idPeticion=idPeticion[1]
-                #                    inicializarpeticon= requests.post(url, headers = cabecera1, data = {"conid":idPeticion,"load":1 })
-                #                    reiniciar = requests.post(url, headers = cabecera1, data = {"conid":idPeticion,"event":5})
-                #                    json_masivo={
-                #                    "conid": idPeticion,
-                #                    "elementFound": 7,
-                #                    "response": "2_llevadero"
-                #                    }                
-                #                    solicitudmasiva = requests.post(url, headers = cabecera1, data = json_masivo)
-                #                    
-                #                reiniciar = requests.post(url, headers = cabecera1, data = {"conid":idPeticion,"event":5})
-                #
-                #                idregistroc=str(idregistro)+"_"+token.lemma_
-                #                obtener_palabra=requests.post(url, headers = cabecera1, data = {"conid":idPeticion,"elementFound":elementFound,"response":idregistroc})
-                #            
-                #                idregistro=idregistro+1
-                #                idregistroc=str(idregistro)+"_"+token.lemma_
-                #                
-                #                obtener_palabra=requests.post(url, headers = cabecera1, data = {"conid":idPeticion,"elementFound":elementFound,"response":idregistroc})
-                #                #se obtiene lo necseairo del servicio y se elimina codigo basura
-                #                lemmaService=fn_limpiarPalabra(obtener_palabra.text)
-                #                
-                #                if lemmaService:
-                #                   token_tmp["lemma"]=lemmaService["lemma"]
-                #                   token_tmp["tipo"]=lemmaService["categoria"]
-                ##                   if token_tmp["tipo"]=="Sin catalogar":
-                ##                       corregirPalabra=requests.post("https://languagetool.org/api/v2/check", headers = cabecera1, data = {"disabledRules": "WHITESPACE_RULE","allowIncompleteResults":True,"text":token_tmp["texto"],"language": "es"})
-                ##                       corregirPalabra=corregirPalabra.text
-                ##                       print(corregirPalabra.text.matches[0].replacements[0].value)
-                #                       
-                #                elementFound=elementFound+1
-                            aFraseLematizada.append(token_tmp)
-                            aLemmasOracion.append(token_tmp["lemma"])    
-                            #print(token_tmp["texto"], token_tmp["lemma"], token_tmp["tipo"])
+                for token in nlp(' '.join(aFrase_sin_stopwords)):
+                    iPeticionWebFirstTime=iPeticionWebFirstTime+1
+                    token_tmp={"texto":token}
+                    if iPeticionWebFirstTime==1:
+                        getIdPeticion= requests.post(url, headers = cabecera1, data = {"url":"/lematizador.html","version":"2.1.1","jaxurl":"JaxcentServlet" })
+                        idPeticion=getIdPeticion.text.split("&")
+                        idPeticion=idPeticion[1]
+                        inicializarpeticon= requests.post(url, headers = cabecera1, data = {"conid":idPeticion,"load":1 })
+                        reiniciar = requests.post(url, headers = cabecera1, data = {"conid":idPeticion,"event":5})
+                        json_masivo={
+                        "conid": idPeticion,
+                        "elementFound": 7,
+                        "response": "2_llevadero"
+                        }                
+                        solicitudmasiva = requests.post(url, headers = cabecera1, data = json_masivo)
+                        
+                    reiniciar = requests.post(url, headers = cabecera1, data = {"conid":idPeticion,"event":5})
+    
+                    idregistroc=str(idregistro)+"_"+str(token)
+                    obtener_palabra=requests.post(url, headers = cabecera1, data = {"conid":idPeticion,"elementFound":elementFound,"response":idregistroc})
                 
-                    else:
-                        if token.text=="!":
-                            aFraseLematizada.append({"texto":token.text, "lemma":token.lemma_, "tipo":token.pos_ })
-                
+                    idregistro=idregistro+1
+                    idregistroc=str(idregistro)+"_"+str(token)
+                    
+                    obtener_palabra=requests.post(url, headers = cabecera1, data = {"conid":idPeticion,"elementFound":elementFound,"response":idregistroc})
+                    #se obtiene lo necseairo del servicio y se elimina codigo basura
+                    lemmaService=fn_limpiarPalabra(obtener_palabra.text)
+                    print(lemmaService)
+                    if lemmaService:
+                       token_tmp["lemma"]=lemmaService["lemma"]
+                       token_tmp["tipo"]=lemmaService["categoria"]
+    #                   if token_tmp["tipo"]=="Sin catalogar":
+    #                       corregirPalabra=requests.post("https://languagetool.org/api/v2/check", headers = cabecera1, data = {"disabledRules": "WHITESPACE_RULE","allowIncompleteResults":True,"text":token_tmp["texto"],"language": "es"})
+    #                       corregirPalabra=corregirPalabra.text
+    #                       print(corregirPalabra.text.matches[0].replacements[0].value)
+                           
+                    elementFound=elementFound+1
+                aFraseLematizada.append(token_tmp)
+                aLemmasOracion.append(token_tmp["lemma"])    
+                            #print(token_tmp["texto"], token_tmp["lemma"], token_tmp["tipo"])                                
                 aBigrama=(list(nltk.bigrams(aLemmasOracion)))
                 freq = nltk.FreqDist(aLemmasOracion)
                 #for key,val in freq.items():
