689579dd91d19f8a758a9e9b1c06d42c462fa1f2
prjchec.yvelez@umanizales.edu.co <prjchec.yvelez@umanizales.edu.co>
2019-03-19 18:32:25 -0500

    fds

--- src/pages/menu/menu.component.ts
+++ src/pages/menu/menu.component.ts
@@ -18,131 +18,59 @@ fn_dashboard = new EventEmitter()
 aFuentes:any=
   [
     {
-      "nombre":"Agenda",
-      "icon": "/assets/img/agenda.png",
-      "descripcion":"Fechas de Corte Ciclos"
+      "fuente":"Indicadores",
+      "fuente_hijos": 
+        [
+          {
+            "hijo_nombre":"hijo 1",
+            "nietos":[
+            ]
+          },
+          {
+            "hijo_nombre":"hijo 2",
+            "nietos":[
+              
+            ]
+          },
+          {
+            "hijo_nombre":"hijo 3",
+            "nietos":[
+              
+            ]
+          }
+        ]
     },
     {
-      "nombre":"Cartera",
-      "icon": "/assets/img/cartera.png",
-      "descripcion":"Valor Cartera / Días Cartera"
-    },
-    {
-      "nombre":"Datos Básicos",
-      "icon": "/assets/img/datos_basicos.png",
-      "descripcion":"Información de Usuarios"
-    },
-    {
-      "nombre":"Clase Servicio",
-      "icon": "/assets/img/clase_servicio.png",
-      "descripcion":"Tipos de Clase de Servicio"
-    },
-    {
-      "nombre":"Pagos",
-      "icon": "/assets/img/pagos.png",
-      "descripcion":"Facturas / Pagos por producto / Valor Pago"
-    },
-    {
-      "nombre":"Consumo Subsistencia",
-      "icon": "/assets/img/consumo_subsistencia.png",
-      "descripcion":"Valor Consumo / Instalaciones Residencial"
-    },
-    {
-      "nombre":"Suspensión Reconexión",
-      "icon": "/assets/img/suspension_reconexion.png",
-      "descripcion":"Fechas de ejecución / Estados"
-    },
+      "fuente":"Indicadores",
+      "fuente_hijos": 
+        [
 
+        ]
+    },
     {
-      "nombre":"PQRs",
-      "icon": "/assets/img/pqr.png",
-      "descripcion":"Tipo de solicitudes / Acciones",
-      "fichero":"./assets/data/pqrs.json",
-      "campos":""
+      "fuente":"Indicadores",
+      "fuente_hijos": 
+        [
+          {
+            "hijo_nombre":"hijo 1",
+            "nietos":[
 
-    },
-
-    {
-      "nombre":"Control Pérdidas",
-      "icon": "/assets/img/control_perdidas.png",
-      "descripcion":"Irregularidades"
-    },
-    {
-      "nombre":"Segmentación",
-      "icon": "/assets/img/segmentacion.png",
-      "descripcion":"Irregularidades"
-    },
-    {
-      "nombre":"Usuarios Circuito",
-      "icon": "/assets/img/usuarios_circuito.png",
-      "descripcion":"Ubicación, Subestación, Circuito, Nodo"
-    },
-    {
-      "nombre":"CIIU",
-      "icon": "/assets/img/ciiu.png",
-      "descripcion":"Actividades Económicas"
-    },
-    {
-      "nombre":"Barrios / Veredas",
-      "icon": "/assets/img/barrios.png",
-      "descripcion":"Caldas - Manizales"
-    },
-    {
-      "nombre":"Cuentas Agrupadas",
-      "icon": "/assets/img/cuentas_agrupadas.png",
-      "descripcion":"Alumbrado Público"
-    },
-    {
-      "nombre":"Gestión Admininfo",
-      "icon": "/assets/img/gestion_admininfo.png",
-      "descripcion":"Contacto Clientes"
-    },
-    {
-      "nombre":"GIIP",
-      "icon": "/assets/img/giip.png",
-      "descripcion":"Balance de Reportes"
-    },
-    {
-      "nombre":"Vanguardistas",
-      "icon": "/assets/img/vanguardistas.png",
-      "descripcion":"Uso de Canalas Vanguardistas"
-    },
-    {
-      "nombre":"Visión Cliente",
-      "icon": "/assets/img/vision_cliente.png",
-      "descripcion":"Arrendatario / Arrendador"
-    },
-    {
-      "nombre":"Turnos",
-      "icon": "/assets/img/turnos.png",
-      "descripcion":"Atención al Cliente"
-    },
-    {
-      "nombre":"Copias Factura",
-      "icon": "/assets/img/copias_factura.png",
-      "descripcion":"Petición copias facturas"
-    },
-    {
-      "nombre":"Facturación",
-      "icon": "/assets/img/facturacion.png",
-      "descripcion":"Usuarios"
-    },
-    {
-      "nombre":"PFS",
-      "icon": "/assets/img/pfs.png",
-      "descripcion":"Planes de Financiación"
-    },
-    {
-      "nombre":"Geografía",
-      "icon": "/assets/img/geografia.png",
-      "descripcion":"Datos de localización Geográfica"
-    },
-    {
-      "nombre":"Indisponibilidades",
-      "icon": "/assets/img/giip.png",
-      "descripcion":"Eventos Fechas"
+            ]
+          },
+          {
+            "hijo_nombre":"hijo 2",
+            "nietos":[
+              
+            ]
+          },
+          {
+            "hijo_nombre":"hijo 3",
+            "nietos":[
+              
+            ]
+          }
+        ]
     }
-
   ]
 
 
